977e7e43dba24114e60620fd466a82a7159a3866
higor-j <103465007+higor-j@users.noreply.github.com>
2022-06-11 08:56:43 -0400

    Add files via upload

---
+++ Cálculo Média de Alunos/Cálculo Média de Alunos/Alunos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Cálculo_Média_de_Alunos {
+    public class Alunos {
+
+        public Double notas;
+
+        public Alunos() {
+        }
+
+        public Alunos(double notas) {
+            this.notas = notas;
+        }
+    }
+}
---
+++ Cálculo Média de Alunos/Cálculo Média de Alunos/Program.cs
@@ -0,0 +1,27 @@
+﻿using Cálculo_Média_de_Alunos;
+
+Alunos[] notas = new Alunos[6];
+
+Alunos camila = new Alunos(8.8);
+Alunos virginia = new Alunos(7.6);
+Alunos pedro = new Alunos(5.3);
+Alunos nestor = new Alunos(4.1);
+Alunos carol = new Alunos(9.1);
+Alunos ringo = new Alunos(0.0);
+
+notas[0] = camila;
+notas[1] = virginia;
+notas[2] = pedro;
+notas[3] = nestor;
+notas[4] = carol;
+notas[5] = ringo;
+
+for (int i = 0; i < notas.Length; i++) {
+    if (notas[i].notas <= 10.0 && notas[i].notas >= 7.0) {
+        Console.WriteLine($"Notas acima de 7.0: {notas[i].notas} \n");
+    } else if (notas[i].notas < 7.0 && notas[i].notas > 0.0) {
+        Console.WriteLine($"Notas abaixo de 7.0: {notas[i].notas} \n");
+    } else {
+        Console.WriteLine($"Nota zerada!: {notas[i].notas}");
+    }
+}
